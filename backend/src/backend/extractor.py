3d7fd7b6cddfe41f86438b2094c93069a082adcb
Brian Kessel <brianskessel@gmail.com>
2024-02-27 16:04:11 -0700

    added some more content to backend

--- backend/src/backend/extractor.py
+++ backend/src/backend/extractor.py
@@ -1,3 +1,17 @@
 # Extract text from file, section it with langchain sectioner, then make langchain documents
 # include metadata in documents
 # use textract
+import textract
+
+class Extractor:
+    # def __init__():
+
+    def extract_to_langchain(self,file: str):
+        content: str = textract.process(file)
+        metadata: dict = {} #include filename
+        # extract
+        # section
+    
+    def content(self,file: str):
+        # just returns the content of the file
+        pass
--- backend/src/backend/fileio.py
+++ backend/src/backend/fileio.py
@@ -4,3 +4,46 @@
 
 
 class FileDB:
+    def __init__(self, 
+                 folder: str,
+                 chroma_dir: str = "chroma.sqlite"
+                 
+                 ) -> None:
+        # the folder that should be watched
+        self.folder: str = folder
+        # the persistent chromadb location, this is going to be an sqlite file
+        self.chroma_dir: str = chroma_dir
+        self.chroma = None # this should be the langchain.chroma instance and should take in chroma_dir
+        self.extractor = None #import extractor
+        self.file_types = ["pdf","txt"] #w
+        # reload() ensure everything upto date on start
+    
+    def reload(self):
+        # this will check every file and add the missing ones
+        # use tqdm to show iteration over all the files ?
+        pass
+
+    def import_file(self,file):
+        # after a file is added to the db this might not be loaded in. Call extract and then add via langchain to the chromadb 
+        # if file is in dir just check to see if in chromadb
+        # if file is not in dir move it into the dir
+        pass
+    
+    def search(self,query):
+        # search filenames, metadata, and vectordb
+        # return langchain objects
+        # maybe filter?
+        pass
+
+    def delete(self,file):
+        #delete file from folder
+        #delete file from chroma
+        pass
+
+    
+
+
+
+    
+
+    
