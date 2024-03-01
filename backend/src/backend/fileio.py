eb0e1fb7d5a4e5f32c49831932f7b5f91c59a3aa
Brian Kessel <brianskessel@gmail.com>
2024-03-01 13:29:44 -0700

    Updated teh searching to actually work with the backend, added file importing, added test

--- backend/src/backend/fileio.py
+++ backend/src/backend/fileio.py
@@ -23,15 +23,16 @@ class FileDB:
         self.extractor = Extractor() #import extractor
         self.file_types = ["pdf","txt"] 
         # reload() ensure everything upto date on start
+        self.sync()
     
     def sync(self):
         # this will check every file and add the missing ones
         # use tqdm to show iteration over all the files ?
         for file in os.listdir(self.folder):
             if file.endswith(tuple(self.file_types)):
-                self.import_file(file)
+                self.sync_file(file)
 
-    def import_file(self,file):
+    def sync_file(self,file):
         documents: List[Documents] = self.extractor.extract_to_documents(file)
         # check if the document was already in chromadb
         potential_docs = self.collection.get(
@@ -41,7 +42,14 @@ class FileDB:
             return
         self.collection.add(documents=[doc.text for doc in documents],metadatas=[doc.metadata for doc in documents], ids=[str(uuid.uuid4()) for _ in documents])
     
-    def search(
+    def add_file(self,file):
+        """
+        Moves a file from the given location to the watched folder, then syncs the file to chromadb
+        """
+        os.rename(file, os.path.join(self.folder, file))
+        self.sync_file(file)
+    
+    def vector_search(
         self,
         query_texts: Optional[List[str]] = None,
         n_results: int = 10,
@@ -55,6 +63,7 @@ class FileDB:
             where=where,
             **kwargs,
         )
+    
 
     def delete(self,file):
         self.collection.delete(
