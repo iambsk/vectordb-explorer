2061be60a473777de11cb48b5621c60c351a88e1
Max <maximilian.strack@student.nmt.edu>
2024-03-01 16:24:16 -0700

    Changed the graphics to a stackedWidget, and added the splitters to the py file

--- frontend/mainwindow.py
+++ frontend/mainwindow.py
@@ -1,5 +1,5 @@
 from PyQt5 import QtCore, QtGui, QtWidgets
-from PyQt5.QtWidgets import QApplication, QMainWindow, QLineEdit, QGraphicsView, QTreeView, QListView, QFileDialog, QMenuBar, QMenu, QAction
+from PyQt5.QtWidgets import QApplication, QMainWindow, QLineEdit, QStackedWidget, QTreeView, QListView, QFileDialog, QMenuBar, QMenu, QAction, QSplitter
 from PyQt5 import uic
 import sys
 
@@ -12,9 +12,10 @@ class UI(QMainWindow):
 
 		# defs from ui file
 		self.searchBar = self.findChild(QLineEdit , "searchBar")
-		self.graphicsView = self.findChild(QGraphicsView, "graphicsView")
+		self.graphicsView = self.findChild(QStackedWidget, "stackedWidget")
 		self.treeView = self.findChild(QTreeView, "treeView")
 		self.listView = self.findChild(QListView, "listView")
+		self.splitter = self.findChild(QSplitter, "splitter")
 
 		self.menubar = self.findChild(QMenuBar, "menubar")
 		self.menuFile = self.findChild(QMenu, "menuFile")
@@ -22,6 +23,7 @@ class UI(QMainWindow):
 		self.menuView = self.findChild(QMenu, "menuView")
 
 		# connect
+		self.splitter.setSizes([50, 100, 50])
 		self.searchBar.setPlaceholderText("Search")
 		self.actionNew.triggered.connect(self.openFileDialog)
 
